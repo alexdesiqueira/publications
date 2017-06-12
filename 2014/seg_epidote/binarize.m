bcdd468df80f3ef0d33b163e5847935552d12241
alexandrejaguar <siqueiraaf@gmail.com>
2017-06-12 17:58:18 -0300

    Improved binarize.m.

--- 2014/seg_epidote/binarize.m
+++ 2014/seg_epidote/binarize.m
@@ -1,37 +1,50 @@
+%%
 %%  BINARIZE.M
 %%
-%%  Version: november 2013.
+%% Copyright (C) 2014 Alexandre Fioravante de Siqueira
+%%
+%% This file is part of the supplementary material to 'An automatic
+%% method for segmentation of fission tracks in epidote crystal
+%% photomicrographs, based on starlet wavelets'.
 %%
 %%  This file is part of the supplementary material to 'An automatic 
 %% method for segmentation of fission tracks in epidote crystal 
 %% photomicrographs, based on starlet wavelets'.
 %%
-%%  Authors: 
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%
+%%    * Authors: 
 %% Alexandre Fioravante de Siqueira, siqueiraaf@gmail.com
 %% Wagner Massayuki Nakasuga, wamassa@gmail.com
 %% Aylton Pagamisse, aylton@fct.unesp.br
 %% Carlos Alberto Tello Saenz, tello@fct.unesp.br
 %% Aldo Eloizo Job, job@fct.unesp.br
-%%	
-%%  Description: this software applies algorithms to segment fission-tracks 
-%% in crystal images by optical microscopy, based on starlets. 
-%% Automatization of these algorithms is given using Matthews Correlation 
-%% Coefficient (MCC). The difference between an image and its Ground 
-%% Truth is given by a colored comparison.
+%%
+%%    * Description: this software applies algorithms to segment
+%% fission-tracks in crystal images by optical microscopy, based on
+%% starlets. Automatization of these algorithms is given using Matthews
+%% Correlation Coefficient (MCC). The difference between an image and
+%% its Ground Truth is given by a colored comparison.
 %%
 %%  Input: IMG, a gray input image.
 %%
 %%  Output: OUT, binarized image output.
 %%
-%%	Other files required: main.m, confusionmatrix.m, mattewscc.m, 
+%%    * Other files required: main.m, confusionmatrix.m, mattewscc.m, 
 %% starlet.m, twodimfilt.m, xtracttracks.m
 %%
-%%  Please cite:
+%%    * Please cite:
 %%
-%% Alexandre Fioravante de Siqueira, Wagner Massayuki Nakasuga, Aylton 
-%% Pagamisse, Carlos Alberto Tello Saenz and Aldo Eloizo Job. An automatic 
-%% method for segmentation of fission tracks in epidote crystal 
-%% photomicrographs, based on starlet wavelets. 2013.
+%% de Siqueira, A. F., Nakasuga, W. M., Pagamisse, A., Tello Saenz, C.
+%% A., & Job, A. E. (2014). An automatic method for segmentation of
+%% fission tracks in epidote crystal photomicrographs. Computers and
+%% Geosciences, 69, 55–61. https://doi.org/10.1016/j.cageo.2014.04.008
 %%
 
 function OUT = binarize(IMG)
